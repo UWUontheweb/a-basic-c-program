8a43dba3ec032245a5cb12a1a48872d77293ebdd
UwUontheweb <94400578+UWUontheweb@users.noreply.github.com>
2021-12-08 11:50:44 -0500

    Add files via upload

---
+++ adding.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+    int numone = 0;
+    int numtwo = 0;
+    
+    std::cout << "enter two numbers...";
+    
+    std::cin >> numone;
+    std::cin >> numtwo;
+    
+    std::cout << "the total is " << numone + numtwo << "\n";
+    
+}
